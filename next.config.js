c0df36dafae10ae4e24924fdb27e3f6c8c26332c
ahmadalfaqih <ahmadamjad277@gmail.com>
2025-11-26 16:08:15 +0300

    chore(config): migrate next.config to JS for compatibility

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
+  reactStrictMode: false,
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
